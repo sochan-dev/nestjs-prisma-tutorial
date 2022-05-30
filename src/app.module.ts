d88be4fbcd322728c462a848a6c2c0f2a7cf7ea7
sotatsu kitajima <halosakakitajima@gmail.com>
2022-05-30 19:12:17 +0900

    create nestjs tutolial for prisma

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostService } from './post/post.service';
+import { PrismaService } from './prisma.service';
+import { UserService } from './user/user.service';
+
+@Module({
+  // imports: [PrismaService],
+  controllers: [AppController],
+  providers: [AppService, PostService, PostService, UserService, PrismaService],
+})
+export class AppModule {}
